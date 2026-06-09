120e4e8d067dca458235237e635d5a26ebf1c02e
恒银 <@>
2026-06-09 11:56:01 +0800

    推送

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,9 @@ Console.WriteLine("Hello, World!");
  void liukehao()
  {
     Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
 }
 void lizhi()
 {
