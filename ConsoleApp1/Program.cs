a4831cafd3bcdf8f00ad7270d3245f150b12a9a0
恒银 <@>
2026-06-09 14:19:29 +0800

    添加劉克豪你好

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,10 @@ Console.WriteLine("Hello, World!");
  void liukehao()
  {
     Console.WriteLine("Hello, World!");
-    Console.WriteLine("Hello, World!");
-    Console.WriteLine("Hello, World!");
+    Console.WriteLine("劉");
+    Console.WriteLine("克");
+    Console.WriteLine("豪");
+    Console.WriteLine("你好");
 }
 void lizhi()
 {
